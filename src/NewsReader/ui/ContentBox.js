5babc869107e7664d79c4f8efacfe15a88b4bef5
Yauhen Brouka <ybrouka@iba.by>
2015-12-01 15:24:33 +0300

    Review change1

--- src/NewsReader/ui/ContentBox.js
+++ src/NewsReader/ui/ContentBox.js
@@ -1,7 +1,7 @@
 NewsReader.ui.ContentBox = React.createClass({
     render: function() {
         return (
-            <div className="row" id="news-reader-content-box">Hey! I'm content box!</div>
+            <div className="row" id="news-reader-content-box"></div>
         );
     }
 });
